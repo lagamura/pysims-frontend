5984c7df8b3bb7c6db788515d29b569268e92ca2
Lagaras Stelios <50661518+lagamura@users.noreply.github.com>
2022-05-10 11:43:02 +0300

    Json Transmission - creation of chartsjs

--- src/views/NewSimulation.vue
+++ src/views/NewSimulation.vue
@@ -3,16 +3,17 @@
       <h3 class="pb-5">Create a new simulation</h3>
     <div class="row mb-3">
       <div class="col-md-4 themed-grid-col">
-      <ListAvailableModels/>
+        <ListAvailableModels/>
       </div>
       <div class="col-md-4 themed-grid-col">
-      <ResultsGraph/>
+        <ResultsGraphVue/>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  import ListAvailableModels from '../components/ListAvailableModels.vue';
-  import ResultsGraph from '../components/ResultsGraph.vue';
+import ListAvailableModels from '../components/ListAvailableModels.vue';
+import ResultsGraph from '../components/ResultsGraph.vue';
+import ResultsGraphVue from '../components/ResultsGraphVue.vue';
 </script>
