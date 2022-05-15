3f54153fcd32071575cf98df3c034470482e8355
Lagaras Stelios <50661518+lagamura@users.noreply.github.com>
2022-05-15 11:44:56 +0300

    last commit before splitting front-end

--- src/components/ResultsGraph.vue
+++ src/components/ResultsGraph.vue
@@ -1,80 +1,74 @@
 <template>
-
-<h3>
-    This is the plot Component
-</h3>
-<canvas id="myChart" width="400" height="400"></canvas>
-
+  <h3>This is the plot Component</h3>
+  <canvas id="myChart" width="400" height="400"></canvas>
 </template>
 
 <script setup>
-import { ref,onMounted } from "@vue/runtime-core";
-import {Chart, registerables} from "chart.js";
-import axios from "axios"
+import { ref, onMounted } from "@vue/runtime-core";
+import { Chart, registerables } from "chart.js";
+import axios from "axios";
 //import Utils from "../../utils.js"
 Chart.register(...registerables);
 
-let json_data = ref([])
+let json_data = ref([]);
 
+onMounted(async () => {
+  // Axios call to get json data
 
-onMounted (async() => {
-
-    // Axios call to get json data
-
-    await axios.get("http://127.0.0.1:8000/get_simul_res_json/1")
-        .then(response => {
-        json_data.value = response.data;
-        console.log(json_data)
-        })
-        .catch(error => {
-        alert(error)
-        })
-})
+  await axios
+    .get("http://127.0.0.1:8000/get_simul_res_json/1")
+    .then((response) => {
+      json_data.value = response.data;
+      console.log(json_data);
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
 
 /* Chart js */
 
 const DATA_COUNT = 7;
-const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
+const NUMBER_CFG = { count: DATA_COUNT, min: -100, max: 100 };
 
-const labels = Utils.months({count: 7});
+const labels = Utils.months({ count: 7 });
 const data = {
   labels: labels,
   datasets: [
     {
-      label: 'Dataset 1',
+      label: "Dataset 1",
       data: Utils.numbers(NUMBER_CFG),
       borderColor: Utils.CHART_COLORS.red,
       backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
     },
     {
-      label: 'Dataset 2',
+      label: "Dataset 2",
       data: Utils.numbers(NUMBER_CFG),
       borderColor: Utils.CHART_COLORS.blue,
       backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-    }
-  ]
+    },
+  ],
 };
 
-onMounted(()=>{
-  var ctx = document.getElementById('myChart').getContext('2d');
+onMounted(() => {
+  var ctx = document.getElementById("myChart").getContext("2d");
   var myChart = new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: data,
     options: {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Chart.js Line Chart'
-            }
-        }
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "Chart.js Line Chart",
+        },
+      },
     },
-      
   });
-})
+});
 
 /* 
 onMounted(()=>{
--- src/views/NewSimulation.vue
+++ src/views/NewSimulation.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="container pt-5">
-      <h3 class="pb-5">Create a new simulation</h3>
+    <div class="d-flex justify-content: center">
+      <!--not working centered-->
+      <h3 class="pb-5">Results of your Simulation</h3>
+    </div>
     <div class="row mb-3">
       <div class="col-md-4 themed-grid-col">
-        <ListAvailableModels/>
+        Here Posts component deactivated for debugging purposes
       </div>
       <div class="col-md-4 themed-grid-col">
-        <ResultsGraphVue/>
+        <p>Line Chart to be implemented</p>
+        <LineChart />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import ListAvailableModels from '../components/ListAvailableModels.vue';
-import ResultsGraph from '../components/ResultsGraph.vue';
-import ResultsGraphVue from '../components/ResultsGraphVue.vue';
+import ResultsGraph from "../components/ResultsGraph.vue";
+import ResultsGraphVue from "../components/ResultsGraphVue.vue";
+import BarChart from "../components/BarChart.vue";
+import LineChart from "../components/LineChart.vue";
+import Posts from "../components/Posts.vue";
 </script>
