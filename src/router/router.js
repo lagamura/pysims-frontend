86219132308335d26146101f24f8b069773174e4
Lagaras Stelios <50661518+lagamura@users.noreply.github.com>
2022-08-18 12:24:36 +0300

    fix glitch with vue router

--- src/router/router.js
+++ src/router/router.js
@@ -36,7 +36,7 @@ const routes = [
     }
 ]
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(),
     routes
 })
 export default router
