749e40d6dc69b074654edef0af157f428128b76a
Lagaras Stelios <50661518+lagamura@users.noreply.github.com>
2022-09-28 19:45:48 +0300

    added authentication, bug with hmr needs to be fixed

--- src/views/StudentDashboardView.vue
+++ src/views/StudentDashboardView.vue
@@ -387,7 +387,7 @@ function formatDate(date) {
   width: 400px;
   padding: 20px;
   border-radius: 20px;
-  border: solid 3px var(--el-color-primary);
+  border: solid 3px var(--el-color-primary-light-3);
   /* flex: 1 0 ; */
   background: white;
   margin: 5px;
